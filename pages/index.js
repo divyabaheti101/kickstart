a1a75ad574138a85841a5c2bc3425de70ec0c61d
Divya Baheti <divya.baheti@fisglobal.com>
2023-04-19 20:25:03 +0530

    getInitialProps()

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,24 @@ import react, { Component } from "react";
 import factory from "../ethereum/factory";
 
 class CampaignIndex extends Component {
-  async componentDidMount() {
-    const campaign = await factory.methods.getDeployedContracts().call();
-
-    console.log(campaign);
+  static async getInitialProps() {
+    const campaigns = await factory.methods.getDeployedContracts().call();
+    return { campaigns };
   }
 
+  //In next if you want to load any data, it should be loaded in this method, instead of
+  //get componentMount as in react
+  //it is static bcoz we don't want to create instance of CampaignIndex just want to render it.
+
+  // async componentDidMount() {
+  //   const campaign = await factory.methods.getDeployedContracts().call();
+
+  //   console.log(campaign);
+  // }
+
   render() {
-    return <div>Okay</div>;
+    return <div>{this.props.campaigns[0]}</div>;
+    // return <div>okay</div>;
   }
 }
 
