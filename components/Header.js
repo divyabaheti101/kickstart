4168b300f644877fa8f72e38c1e8b8f7259e3db9
Divya Baheti <divya.baheti@fisglobal.com>
2023-04-20 19:26:00 +0530

    Containers, styling

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import { Menu } from "semantic-ui-react";
 
 export const Header = () => {
   return (
-    <Menu>
+    <Menu style={{ marginTop: "20px" }}>
       <Menu.Item>CrowdCoin</Menu.Item>
       <Menu.Menu position="right">
         <Menu.Item>Campaigns</Menu.Item>
--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Header } from "./Header";
+import { Container } from "semantic-ui-react";
 
 export default (props) => {
   return (
-    <div>
+    <Container>
       <Header />
       {props.children}
       {/* <h3>Footer<h3/> */}
-    </div>
+    </Container>
   );
 };
