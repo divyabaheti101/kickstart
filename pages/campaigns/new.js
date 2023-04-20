af90baf788bb526f116e8a8a9ed525926bd9c3d0
Divya Baheti <divya.baheti@fisglobal.com>
2023-04-20 19:40:17 +0530

    Nested routing and paths, just create nested folders

---
+++ pages/campaigns/new.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+import React from "react";
+
+class CampaignNew extends Component {
+  render() {
+    return <h1>New Cmapaign</h1>;
+  }
+}
+export default CampaignNew;
