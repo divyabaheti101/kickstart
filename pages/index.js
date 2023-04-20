217f2b6f3dd5e95b6bda6634c0bba11156d9127a
Divya Baheti <divya.baheti@fisglobal.com>
2023-04-20 19:33:50 +0530

    positioning of button

--- pages/index.js
+++ pages/index.js
@@ -41,8 +41,14 @@ class CampaignIndex extends Component {
             href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
           />
           <h3>Open Campaigns</h3>
+
+          <Button
+            floated="right"
+            content="Create Campaign"
+            icon="add circle"
+            primary
+          ></Button>
           {this.renderCampaigns()}
-          <Button content="Create Campaign" icon="add circle" primary></Button>
         </div>
       </Layout>
     );
