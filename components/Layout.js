56b4b149b428a0884f555b4639d7a1bb37969e20
Divya Baheti <divya.baheti@fisglobal.com>
2023-04-20 19:49:37 +0530

    Head, styling to head, next/head

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { Header } from "./Header";
 import { Container } from "semantic-ui-react";
+import Head from "next/head";
+
+//head takes this styling to the head html file.
+//placing this styling at layout as this is used for all pages.
 
 export default (props) => {
   return (
     <Container>
+      <Head>
+        <link
+          async
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+        />
+      </Head>
       <Header />
       {props.children}
       {/* <h3>Footer<h3/> */}
--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,9 +1,14 @@
 import { Component } from "react";
 import React from "react";
+import Layout from "../../components/Layout";
 
 class CampaignNew extends Component {
   render() {
-    return <h1>New Cmapaign</h1>;
+    return (
+      <Layout>
+        <h1>New Cmapaign</h1>
+      </Layout>
+    );
   }
 }
 export default CampaignNew;
