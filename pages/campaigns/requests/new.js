21d4c9203977e643c4b81fe0f98ffb732ba7dc92
Divya Baheti <divya.baheti@fisglobal.com>
2023-04-25 20:06:44 +0530

    new request form created

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -1,8 +1,54 @@
+import { Button, Form, Input } from "semantic-ui-react";
+import Layout from "../../../components/Layout";
+
 const { Component } = require("react");
 
 class RequestNew extends Component {
+  state = {
+    description: "",
+    value: "",
+    reciepient: "",
+  };
+
+  static async getInitialProps(props) {
+    const { address } = props.query;
+    return { address };
+  }
+
   render() {
-    return <h3>Create Request</h3>;
+    return (
+      <Layout>
+        <h3>Create Request</h3>
+        <Form>
+          <Form.Field>
+            <label>Description</label>
+            <Input
+              value={this.state.description}
+              onChange={(event) =>
+                this.setState({ description: event.target.value })
+              }
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Value</label>
+            <Input
+              value={this.state.value}
+              onChange={(event) => this.setState({ value: event.target.value })}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Reciepient</label>
+            <Input
+              value={this.state.reciepient}
+              onChange={(event) =>
+                this.setState({ reciepient: event.target.value })
+              }
+            />
+          </Form.Field>
+          <Button primary>Create!</Button>
+        </Form>
+      </Layout>
+    );
   }
 }
 
