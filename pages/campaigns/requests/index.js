e0f28ddc59cf3216d7023a768764fadf91b47e71
Divya Baheti <divya.baheti@fisglobal.com>
2023-04-26 19:25:52 +0530

    getting all requests

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -2,12 +2,29 @@ import React, { Component } from "react";
 import { Link } from "../../../routes";
 import Layout from "../../../components/Layout";
 import { Button } from "semantic-ui-react";
+import Campaign from "../../../ethereum/campaign";
 
 class RequestIndex extends Component {
   static async getInitialProps(props) {
     const { address } = props.query;
 
-    return { address };
+    const campaign = Campaign(address);
+    const requestCount = await campaign.methods.getRequestsCount().call();
+
+    //Due to Limitation of Solidity
+    //We can get all requests in one go from Solidity as Request is a Struct type.
+    //So we loop thru the requests using count of requests.
+    const requests = await Promise.all(
+      Array(requestCount)
+        .fill()
+        .map((element, index) => {
+          return campaign.methods.requests(index).call();
+        })
+    );
+
+    console.log(requests);
+
+    return { address, requests };
   }
 
   render() {
