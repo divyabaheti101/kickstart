55b7e85a81364e7d29107a08423cf40d87e8ca10
Divya Baheti <divya.baheti@fisglobal.com>
2023-04-26 20:44:10 +0530

    approving request

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -1,8 +1,18 @@
-import { Table } from "semantic-ui-react";
+import { Button, Table } from "semantic-ui-react";
+import Campaign from "../ethereum/campaign";
+import web3 from "../ethereum/web3";
 
 const { Component } = require("react");
 
 class RequestRow extends Component {
+  onApprove = async () => {
+    const campaign = await Campaign(this.props.address);
+    const accounts = await web3.eth.getAccounts();
+
+    await campaign.methods.approveRequest(this.props.id).send({
+      from: accounts[0],
+    });
+  };
   render() {
     const { Row, Cell } = Table;
     const { id, request, approversCount } = this.props;
@@ -10,10 +20,15 @@ class RequestRow extends Component {
       <Row>
         <Cell>{id}</Cell>
         <Cell>{request.description}</Cell>
-        <Cell>{request.value}</Cell>
+        <Cell>{web3.utils.fromWei(request.value, "ether")}</Cell>
         <Cell>{request.reciepient}</Cell>
         <Cell>
           {request.approvalCount} / {approversCount}
+        </Cell>
+        <Cell>
+          <Button color="green" basic onClick={this.onApprove}>
+            Approve
+          </Button>
         </Cell>
       </Row>
     );
